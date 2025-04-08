f91b8c017ce87bd6f452caa90e7b5929ff7676d9
Enes Avsar <e.avsar@optadata-gruppe.de>
2025-04-08 13:54:04 +0200

    added starter. for KB for system start

--- ml_model/evaluate_model.py
+++ ml_model/evaluate_model.py
@@ -19,7 +19,7 @@ y = df["thema"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Vektorisieren | Wandelt Text (Fragen) in Zahlen um, damit ein ML-Modell damit rechnen kann
-# TF-IDF: Term Frequency-Inverse Document Frequency
+# TF-IDF=  Term Frequency-Inverse Document Frequency
 #wörter werden in Vektoren umgewandelt, die Häufigkeit der Wörter in den Fragen wird berücksichtigt
 vectorizer = TfidfVectorizer()
 X_train_vec = vectorizer.fit_transform(X_train)
